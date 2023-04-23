7338853077c5716531b1d570d78acad0075defe7
GhostMaster69-dev <rathore6375@gmail.com>
2023-04-23 09:06:32 +0530

    Tracking now clang-r468909b so use it as default
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- cc/config/global.go
+++ cc/config/global.go
@@ -286,8 +286,8 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r450784d"
-	ClangDefaultShortVersion = "14.0.6"
+	ClangDefaultVersion      = "clang-r468909b"
+	ClangDefaultShortVersion = "15.0.3"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
